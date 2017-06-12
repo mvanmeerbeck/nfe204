443ed0ecd2888bd97840c4b11eafe48be3b602b0
Maxime Vanmeerbeck <mvanmeerbeck@shopbot-inc.com>
2017-06-12 14:07:46 +0200

    bulk exception

--- src/Command/EsImportCommand.php
+++ src/Command/EsImportCommand.php
@@ -37,26 +37,43 @@ class EsImportCommand extends Command
         $i = 0;
 
         $documents = ['body' => []];
-        while ($line = fgets($handle, 1024)) {
+        while ($line = fgets($handle, 2048)) {
+            $document = json_decode($line, true);
+
             $documents['body'][] = [
                 'index' => [
-                    '_index' => 'product',
+                    '_index' => $input->getArgument('type'),
                     '_type' => 'document',
-                    '_id' => $i
+                    '_id' => $document[$input->getArgument('type') . '_id']
                 ]
             ];
 
-            $documents['body'][] = json_decode($line, true);
-
-            if (0 === $i % 1000) {
-                $this->client->bulk($documents);
-
-                $documents = ['body' => []];
-            }
+            $documents['body'][] = $document;
 
             $i++;
+
+            if (0 === $i % 1000) {
+                $this->bulk($documents);
+
+                $documents = ['body' => []];
+                $output->writeln($i . ' documents inserted');
+            }
         }
 
-        $this->client->bulk($documents);
+        $this->bulk($documents);
+        $output->writeln($i . ' documents inserted');
+    }
+
+    private function bulk(array $documents)
+    {
+        $result = $this->client->bulk($documents);
+
+        if ($result['errors']) {
+            foreach ($result['items'] as $item) {
+                if (isset($item['index']['error'])) {
+                    throw new \Exception(sprintf('Document %d failed to be inserted: %s', $item['index']['_id'], $item['index']['error']['reason']));
+                }
+            }
+        }
     }
 }
